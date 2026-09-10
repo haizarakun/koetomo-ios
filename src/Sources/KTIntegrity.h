bb67366286850b6b93ea74a005c62f67e5cc3e42
haizarakun <245811423+haizarakun@users.noreply.github.com>
2026-09-10 10:12:39 +0900

    同梱資産の照合表を更新

--- src/Sources/KTIntegrity.h
+++ src/Sources/KTIntegrity.h
@@ -2,13 +2,13 @@
 #define KT_EXPECTED_VERSION "1.05"
 static const struct { const char *path; const char *sha256; } kKTIntegrity[] = {
     { "android-bridge.js", "3066974fa1fa713b238d05a184f77fc0891ace216451159e9c996609dd949fb3" },
-    { "app.js", "7c1688b124ab1c32f8a2b0a9a6d6d81ecd3fd36a9427a58485ffe27b8269c2b2" },
+    { "app.js", "e5d9ca7bbfa59ebced702f9ed4b33269647a5258195858bfb0424ef7014b9cf8" },
     { "banlist-client.js", "09be4a8515f95f80600092b91c064e919e958a4ac889e0a1b6c540db192c2364" },
-    { "index.html", "d0c5897b2aa432b02d273be7e345d0b56bcb804dfee5c17f00c6951eff5f9d66" },
+    { "index.html", "1077a2af763b63dc9081c92d7d9dfd7bbef36f03252f5944b6a6a85498e4dbf4" },
     { "ios/ios-bridge.js", "0776cb6030e6f3d2bc7dc0a0185b7ab0fd5285d96f42455cc68016d2407267f6" },
     { "ios/ios-session-ext3.js", "916a1770cd262fd527c4e19bd7328538f0a5a952e4ce90ef887a7b94a0b53990" },
     { "ios/ios-session-ext4.js", "ac4db4ed70388cdd6597dfb9ad74c57ab75275aac0378e5ef117aceb5b4f8360" },
-    { "ios/ios-session-ext5.js", "9ba4f88bc28d9b764276ea95dede373b4173aa40944c5bfa497082e88b550660" },
+    { "ios/ios-session-ext5.js", "f3b7fd4f3c70c10b252b7b62c6bc2781ae5d28589462e4d3677e51a78de4ab3d" },
     { "ios/ios-session-ext6.js", "1bc5521a1ccf749311299747f6ce83d5ec5021752a2507de790fee75115aeb07" },
     { "ios/ios-session.js", "80ee919f937643a7c5db3b21351b7a447e7c24c38d8860ac00ef164fe44e1b03" },
     { "logo.png", "ae930e5b751445e5d84ac0d7659fa68e0d9098bc4bed89539a895016c0c38470" },
